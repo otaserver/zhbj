6709b04c9e748669ea527d5cdc7b9583e8bd8816
scott <qvis.be@gmail.com>
2019-11-08 14:44:03 +0800

    修改过时的方法 用Log.X()代替System.out.println()

--- app/src/main/java/com/daleyzou/zhbj/NewsDetailActivity.java
+++ app/src/main/java/com/daleyzou/zhbj/NewsDetailActivity.java
@@ -1,6 +1,5 @@
 package com.daleyzou.zhbj;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentUris;
@@ -14,20 +13,15 @@ import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.webkit.DownloadListener;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -36,13 +30,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.daleyzou.zhbj.domain.Comment;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.mob.MobSDK;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import cn.sharesdk.onekeyshare.OnekeyShare;
@@ -52,6 +48,9 @@ import cn.sharesdk.onekeyshare.OnekeyShareTheme;
  * 新闻详情页面
  */
 public class NewsDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //为日志Log定义tag标签
+    private static final String TAG = "NewsDetailActivity";
 
     @ViewInject(R.id.ll_control)
     private LinearLayout llControl;
@@ -76,7 +75,9 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
     TextView mAmTvComment;
     //添加listview头
     View mView;
-    //列表数据
+    /**
+     * 列表数据
+     */
     List<Comment> mCommentList = new ArrayList<Comment>();
     @ViewInject(R.id.am_et_msg)
     EditText mAmEtMsg;
@@ -90,8 +91,9 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
 
     //回复的内容
     String info = "";
+
     //标记位，是评论还是回复。默true认评论
-    boolean isComment=true;
+    boolean isComment = true;
 
     List<String> coments = new ArrayList<String>();
 
@@ -107,7 +109,6 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         btnTextSize.setVisibility(View.VISIBLE);
         btnMenu.setVisibility(View.GONE);
 
-
         btnBack.setOnClickListener(this);
         btnTextSize.setOnClickListener(this);
         btnShare.setOnClickListener(this);
@@ -115,17 +116,18 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         mAmTvComment.setOnClickListener(this);
         mAmBSave.setOnClickListener(this);
 
-
-
 //        mWebView.loadUrl("http://10.0.2.2:8080/zhbj/10007/724D6A55496A11726628.html");
         WebSettings settings = mWebView.getSettings();
-        settings.setBuiltInZoomControls(true);// 显示缩放按钮
-        settings.setUseWideViewPort(true); // 支持双击缩放
+        // 显示缩放按钮
+        settings.setBuiltInZoomControls(true);
+        // 支持双击缩放
+        settings.setUseWideViewPort(true);
 //        settings.setJavaScriptEnabled(true);// 支持js功能
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAppCacheEnabled(true);
         settings.setDefaultFontSize(600);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
@@ -133,7 +135,7 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         mNewsId = getIntent().getStringExtra("newsId");
 //        mWebView.loadUrl("https://www.baidu.com");
         mWebView.loadUrl(mUrl);
-        mWebView.setWebViewClient(new WebViewClient(){
+        mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {// 开始加载网页
                 super.onPageStarted(view, url, favicon);
@@ -148,7 +150,7 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                System.out.println("跳转链接："+ url);
+                Log.d(TAG, "跳转链接：" + url);
                 view.loadUrl(url);// 强制在当前webview中加载
                 return true;
             }
@@ -158,18 +160,19 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
                 handler.proceed();// 接受所有网站的证书
             }
         });
+
 //        mWebView.goBack();// 跳到上一页面
 //        mWebView.goForward();// 跳到下一页面
-        mWebView.setWebChromeClient(new WebChromeClient(){
+        mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {// 进度发送变化
-                System.out.println("进度：" + newProgress);
+                Log.d(TAG, "进度：" + newProgress);
                 super.onProgressChanged(view, newProgress);
             }
 
             @Override
             public void onReceivedTitle(WebView view, String title) {// 网页标题
-                System.out.println("网页标题:" + title);
+                Log.d(TAG, "网页标题:" + title);
                 super.onReceivedTitle(view, title);
             }
         });
@@ -177,7 +180,7 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_back:
                 finish();
                 break;
@@ -189,16 +192,12 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
                 break;
             case R.id.am_tv_comment:
                 //点击评论按钮
-                System.out.println("****************************************************");
-                System.out.println("已点击评论按钮");
-                System.out.println("****************************************************");
+                Log.d(TAG, "已点击评论按钮");
                 comment(true);
                 break;
             case R.id.am_b_save:
                 // 跳到查看评论详情页面
-                System.out.println("****************************************************");
-                System.out.println("已在保存数据");
-                System.out.println("****************************************************");
+                Log.d(TAG, "已在保存数据");
                 comment(false);
                 saveComment();
                 break;
@@ -211,15 +210,18 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         }
     }
 
-    private int mTempWhich;// 记录临时选择的字体大小（点击确定之前）
-    private int mCurrentWhich; // 记录当前选中的字体大小（点击确定之后）
+    // 记录临时选择的字体大小（点击确定之前）
+    private int mTempWhich;
+    // 记录当前选中的字体大小（点击确定之后）
+    private int mCurrentWhich;
+
     /**
      * 展示选择字体大小的弹窗
      */
     private void showChooseDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("字体设置");
-        String[] items = new String[]{"超大号字体","大号字体","正常字体","小号字体","超小号字体"};
+        String[] items = new String[]{"超大号字体", "大号字体", "正常字体", "小号字体", "超小号字体"};
 
         builder.setSingleChoiceItems(items, mCurrentWhich, new DialogInterface.OnClickListener() {
             @Override
@@ -227,37 +229,38 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
                 mTempWhich = which;
             }
         });
-        builder.setPositiveButton("确定",new DialogInterface.OnClickListener(){
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 WebSettings settings = mWebView.getSettings();
                 // 根据被选择的字体来修改网页字体大小
-                switch (mTempWhich){
+                switch (mTempWhich) {
+                    //文字可以设置5中字体大小格式，分别代表了不同的百分比，分为
+                    //SMALLEST is 50%
+                    //SMALLER is 75%
+                    //NORMAL is 100%
+                    //LARGER is 150%
+                    //LARGEST is 200%
                     case 0:
                         // 超大字体
-//                        settings.setDefaultFontSize(800);
-                         settings.setTextSize(WebSettings.TextSize.LARGEST);
+                        settings.setTextZoom(200);
                         break;
                     case 1:
                         // 大号字体
-//                        settings.setDefaultFontSize(700);
-                        settings.setTextSize(WebSettings.TextSize.LARGER);
+                        settings.setTextZoom(150);
                         break;
                     case 2:
                         // 正常字体
-//                        settings.setDefaultFontSize(600);
-                        settings.setTextSize(WebSettings.TextSize.NORMAL);
+                        settings.setTextZoom(100);
                         break;
                     case 3:
                         // 小号字体
-//                        settings.setDefaultFontSize(500);
-                        settings.setTextSize(WebSettings.TextSize.SMALLER);
+                        settings.setTextZoom(75);
                         break;
                     case 4:
                         // 超小号字体
-//                        settings.setDefaultFontSize(400);
-                        settings.setTextSize(WebSettings.TextSize.SMALLEST);
+                        settings.setTextZoom(50);
                         break;
                     default:
                         break;
@@ -265,13 +268,16 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
                 mCurrentWhich = mTempWhich;
             }
         });
-        builder.setNegativeButton("取消",null);
+        builder.setNegativeButton("取消", null);
         builder.show();
     }
+
     private void showShare() {
+        //mobsdk做的是一键分享功能。但分享微信报未审核应用的提示。
         MobSDK.init(this);
         OnekeyShare oks = new OnekeyShare();
         oks.setTheme(OnekeyShareTheme.CLASSIC);
+
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
 
@@ -282,7 +288,8 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         // text是分享文本，所有平台都需要这个字段
         oks.setText("我是分享文本");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // 确保SDcard下面存在此张图片
+        oks.setImagePath("/sdcard/test.jpg");
         // url在微信、微博，Facebook等平台中使用
         oks.setUrl("http://sharesdk.cn");
         // comment是我对这条分享的评论，仅在人人网使用
@@ -290,25 +297,25 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
         // 启动分享GUI
         oks.show(this);
     }
-    // 保存评论
+
+    /**
+     * 保存评论到本地库中
+     */
     private void saveComment() {
         if (!TextUtils.isEmpty(mAmEtMsg.getText())) {
             info = mAmEtMsg.getText().toString();
-            System.out.println("****************************************************");
-            System.out.println("内容："+info);
-            System.out.println("****************************************************");
-            /**
-             * 将数据插入到我本地的数据库
-             */
+            Log.d(TAG, "评论内容：" + info);
+
+            // 将数据插入到我本地的数据库
             ContentResolver cr = NewsDetailActivity.this.getContentResolver();
             Uri uri = Uri.parse("content://com.daleyzou.zhbj/news_table");
             ContentValues values = new ContentValues();
-            values.put("news_title",mNewsId);
-            values.put("news_content",info);
+            values.put("news_title", mNewsId);
+            values.put("news_content", info);
 
-            Uri result = cr.insert(uri,values);
-            if (ContentUris.parseId(result) > 0){
-                Toast.makeText(NewsDetailActivity.this,"数据增加成功！",Toast.LENGTH_SHORT).show();
+            Uri result = cr.insert(uri, values);
+            if (ContentUris.parseId(result) > 0) {
+                Toast.makeText(NewsDetailActivity.this, "数据增加成功！", Toast.LENGTH_SHORT).show();
             }
             //还原
             comment(false);
@@ -316,18 +323,17 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
             Toast.makeText(NewsDetailActivity.this, "请输入内容后在留言", Toast.LENGTH_SHORT).show();
         }
     }
+
     private void comment(boolean flag) {
-        if(flag){
+        if (flag) {
             mAmLlInfo.setVisibility(View.GONE);
             mAmLlLiuyan.setVisibility(View.VISIBLE);
             onFocusChange(flag);
-        }else{
+        } else {
             mAmLlInfo.setVisibility(View.VISIBLE);
             mAmLlLiuyan.setVisibility(View.GONE);
             onFocusChange(flag);
         }
-
-
     }
 
     /**
@@ -336,6 +342,8 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
     private void onFocusChange(boolean hasFocus) {
         final boolean isFocus = hasFocus;
         (new Handler()).postDelayed(new Runnable() {
+
+            @Override
             public void run() {
                 InputMethodManager imm = (InputMethodManager)
                         NewsDetailActivity.this.getSystemService(INPUT_METHOD_SERVICE);
@@ -355,25 +363,25 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
     /**
      * 从数据库中取出当前newsId对应的评论信息，并传递到CommentActivity中去
      */
-    private void getCommentsList(){
-        // com.daleyzou.zhbj
+    private void getCommentsList() {
         ContentResolver cr = NewsDetailActivity.this.getContentResolver();
         Uri uri = Uri.parse("content://com.daleyzou.zhbj/news_table");
+        Cursor c = cr.query(uri, null, null, null, null);
 
-        Cursor c = cr.query(uri,null,null,null,null);
-        for (c.moveToFirst();!c.isAfterLast();c.moveToNext()){
-            System.out.println("评论newsId："+c.getString(1)+", 评论内容"+c.getString(2));
-            if (c.getString(1).equals(mNewsId)){
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            Log.d(TAG, "评论newsId：" + c.getString(1) + ", 评论内容：" + c.getString(2));
+            if (c.getString(1).equals(mNewsId)) {
                 coments.add(c.getString(2));
             }
-
         }
 
         Intent intent = new Intent(getApplicationContext(), CommentActivity.class);
-        Bundle b=new Bundle();
+        Bundle b = new Bundle();
         b.putStringArrayList("coments", (ArrayList<String>) coments);
         intent.putExtras(b);
         NewsDetailActivity.this.startActivity(intent);
     }
+
+
 }
 
