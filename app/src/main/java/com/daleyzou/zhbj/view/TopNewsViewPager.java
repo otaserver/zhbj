f033f0b69ae82759252b47dbe359c9fa2193e766
scott <qvis.be@gmail.com>
2019-11-19 15:29:18 +0800

    Appcompat库迁移到androidX

--- app/src/main/java/com/daleyzou/zhbj/view/TopNewsViewPager.java
+++ app/src/main/java/com/daleyzou/zhbj/view/TopNewsViewPager.java
@@ -1,11 +1,12 @@
 package com.daleyzou.zhbj.view;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
 
 /**
  * 头条新闻自定义viewpager
@@ -27,6 +28,7 @@ public class TopNewsViewPager extends ViewPager {
      * 1.上下滑动不拦截
      * 2.向右滑动并且且当前是第一个页面，需要拦截
      * 3.向左滑动且当前是最后一个页面，需要拦截
+     *
      * @param ev
      * @return
      */
@@ -34,7 +36,7 @@ public class TopNewsViewPager extends ViewPager {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         getParent().requestDisallowInterceptTouchEvent(true);
 
-        switch (ev.getAction()){
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 startX = (int) ev.getX();
                 startY = (int) ev.getY();
@@ -45,24 +47,24 @@ public class TopNewsViewPager extends ViewPager {
 
                 int dx = endX - startX;
                 int dy = endY - startY;
-                if (Math.abs(dy) < Math.abs(dx)){
+                if (Math.abs(dy) < Math.abs(dx)) {
                     int currentItem = getCurrentItem();
                     // 左右滑动
-                    if (dx > 0){
+                    if (dx > 0) {
                         // 向右划
-                        if (currentItem == 0){
+                        if (currentItem == 0) {
                             // 第一个页面
                             getParent().requestDisallowInterceptTouchEvent(false);
                         }
-                    }else {
+                    } else {
                         // 向左滑
                         int count = getAdapter().getCount(); // item的总数
-                        if (currentItem == count - 1){
+                        if (currentItem == count - 1) {
                             // 最后一个页面，需要拦截
                             getParent().requestDisallowInterceptTouchEvent(false);
                         }
                     }
-                }else {
+                } else {
                     // 上下滑动,要拦截
                     getParent().requestDisallowInterceptTouchEvent(false);
                 }
