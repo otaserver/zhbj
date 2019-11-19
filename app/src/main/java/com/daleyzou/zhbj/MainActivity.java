8a989876edcf6b5354e21071c1ffa8fda2370e59
scott <qvis.be@gmail.com>
2019-11-19 15:48:34 +0800

    Appcompat库迁移到androidX

--- app/src/main/java/com/daleyzou/zhbj/MainActivity.java
+++ app/src/main/java/com/daleyzou/zhbj/MainActivity.java
@@ -1,12 +1,11 @@
 package com.daleyzou.zhbj;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.daleyzou.zhbj.fragment.ContentFragment;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
@@ -28,29 +27,33 @@ public class MainActivity extends SlidingFragmentActivity {
 
         setBehindContentView(R.layout.left_menu);
         SlidingMenu slidingMenu = getSlidingMenu();
-        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);//全屏触摸
-        slidingMenu.setBehindOffset(450);//屏幕预留100像素宽度
-
+        //全屏触摸
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //屏幕预留100像素宽度
+        slidingMenu.setBehindOffset(450);
         initFragment();
     }
 
     /**
      * 初始化Fragment
      */
-    private void initFragment(){
+    private void initFragment() {
         FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();//开启事务
+        //开启事务
+        FragmentTransaction transaction = fm.beginTransaction();
         // 参1：帧布局容器的id；参2：要替换的fragment；参3：要打上的标签的标签名
-        transaction.replace(R.id.fl_main,new ContentFragment(),TAG_CONTENT);
-        transaction.commitNow();//提交事务
+        transaction.replace(R.id.fl_main, new ContentFragment(), TAG_CONTENT);
+        //提交事务
+        transaction.commitNow();
     }
 
 
     /**
      * 获取内容Fragment对象
+     *
      * @return
      */
-    public ContentFragment getContentFragment(){
+    public ContentFragment getContentFragment() {
         FragmentManager fm = getSupportFragmentManager();
         ContentFragment contentFragment = (ContentFragment) fm.findFragmentByTag(TAG_CONTENT);
         return contentFragment;
