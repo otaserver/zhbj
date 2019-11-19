f0fb2ec37d793a898a049a691832f9b52902691c
scott <qvis.be@gmail.com>
2019-11-19 15:53:36 +0800

    javadoc注释

--- app/src/main/java/com/daleyzou/zhbj/NewsSQLiteOpenHelper.java
+++ app/src/main/java/com/daleyzou/zhbj/NewsSQLiteOpenHelper.java
@@ -1,12 +1,9 @@
 package com.daleyzou.zhbj;
 
-import android.app.Activity;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 public class NewsSQLiteOpenHelper extends SQLiteOpenHelper {
 
--- app/src/main/java/com/daleyzou/zhbj/SplashActivity.java
+++ app/src/main/java/com/daleyzou/zhbj/SplashActivity.java
@@ -1,10 +1,7 @@
 package com.daleyzou.zhbj;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
@@ -12,32 +9,44 @@ import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.daleyzou.zhbj.utils.PrefUtils;
 
+/**
+ * 启动页，也是app的主活动页
+ */
 public class SplashActivity extends AppCompatActivity {
 
     private RelativeLayout rlRoot;
+
+    /**
+     * 是否显示开屏动画。
+     */
+//    private boolean isShowSplash = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
-        rlRoot = (RelativeLayout)findViewById(R.id.rl_root);
+        rlRoot = (RelativeLayout) findViewById(R.id.rl_root);
         // 旋转动画
-        RotateAnimation animRotate = new RotateAnimation(0,360,
-                Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        animRotate.setDuration(1000);//旋转时间
-        animRotate.setFillAfter(true);// 保持动画结束状态
+        RotateAnimation animRotate = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        //旋转时间
+        animRotate.setDuration(100);
+        // 保持动画结束状态
+        animRotate.setFillAfter(true);
 
         //缩放动画
-        ScaleAnimation animScale = new ScaleAnimation(0,1,0,1,
-                Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        animScale.setDuration(1000);
+        ScaleAnimation animScale = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animScale.setDuration(100);
 
         // 渐变动画
-        AlphaAnimation animAlpha = new AlphaAnimation(0,1);
-        animAlpha.setDuration(2000);
+        AlphaAnimation animAlpha = new AlphaAnimation(0, 1);
+        animAlpha.setDuration(200);
         animAlpha.setFillAfter(true);
 
         // 动画集合
@@ -47,7 +56,9 @@ public class SplashActivity extends AppCompatActivity {
         set.addAnimation(animAlpha);
 
         // 启动动画
+//        if(isShowSplash){
         rlRoot.startAnimation(set);
+//        }
 
         set.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -62,11 +73,11 @@ public class SplashActivity extends AppCompatActivity {
                 // 否则跳主页面
 
                 Intent intent;
-                boolean isFirstEnter = PrefUtils.getBoolean(SplashActivity.this,"is_first_enter",true);
-                if (isFirstEnter){
+                boolean isFirstEnter = PrefUtils.getBoolean(SplashActivity.this, "is_first_enter", true);
+                if (isFirstEnter) {
                     //新手引导
                     intent = new Intent(getApplicationContext(), GuideActivity.class);
-                }else {
+                } else {
                     // 主页面
                     intent = new Intent(getApplicationContext(), MainActivity.class);
                 }
