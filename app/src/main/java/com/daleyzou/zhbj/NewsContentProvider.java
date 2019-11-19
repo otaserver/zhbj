b50db541a7ab215fd86fd517bfad9cfd295d2309
scott <qvis.be@gmail.com>
2019-11-19 15:50:06 +0800

    Appcompat库迁移到androidX

--- app/src/main/java/com/daleyzou/zhbj/NewsContentProvider.java
+++ app/src/main/java/com/daleyzou/zhbj/NewsContentProvider.java
@@ -3,33 +3,33 @@ package com.daleyzou.zhbj;
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class NewsContentProvider extends ContentProvider {
 
+    private static final String TAG = "NewsContentProvider";
+
     private NewsSQLiteOpenHelper dbHelper = null;
 
-    private static final UriMatcher MATCHER =  new UriMatcher(UriMatcher.NO_MATCH);
-    private  static final int BOOK_TABLE = 1;
-    static {
+    private static final UriMatcher MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+    private static final int BOOK_TABLE = 1;
 
-        MATCHER.addURI("com.daleyzou.zhbj","news_table",BOOK_TABLE);
+    static {
+        MATCHER.addURI("com.daleyzou.zhbj", "news_table", BOOK_TABLE);
     }
 
 
     @Override
     public boolean onCreate() {
-        Log.d("Test","NewsContentProvider - onCreate");
-        dbHelper = new NewsSQLiteOpenHelper(this.getContext(),"news.db",null,1);
+        Log.d(TAG, "NewsContentProvider - onCreate");
+        dbHelper = new NewsSQLiteOpenHelper(this.getContext(), "news.db", null, 1);
         return false;
     }
 
@@ -42,18 +42,17 @@ public class NewsContentProvider extends ContentProvider {
                 return db.query("news_table", projection, selection, selectionArgs, null, null, sortOrder);
             default:
                 throw new IllegalArgumentException("Unknown Uri:" + uri.toString());
-
         }
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        switch (MATCHER.match(uri)){
+        switch (MATCHER.match(uri)) {
             case BOOK_TABLE:
                 return "vnd.android.cursor.dir/news_table";
             default:
-                throw new IllegalArgumentException("Unknown Uri:"+uri.toString());
+                throw new IllegalArgumentException("Unknown Uri:" + uri.toString());
         }
     }
 
@@ -62,13 +61,13 @@ public class NewsContentProvider extends ContentProvider {
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         Uri insertUri = null;
-        switch (MATCHER.match(uri)){
+        switch (MATCHER.match(uri)) {
             case BOOK_TABLE:
                 long rowId = db.insert("news_table", "", values);
-                insertUri = ContentUris.withAppendedId(uri,rowId);
+                insertUri = ContentUris.withAppendedId(uri, rowId);
                 return insertUri;
             default:
-                throw new IllegalArgumentException("Unknown Uri:"+uri.toString());
+                throw new IllegalArgumentException("Unknown Uri:" + uri.toString());
         }
     }
 
