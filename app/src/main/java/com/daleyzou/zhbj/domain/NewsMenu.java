0aa45ef3c78e938b2c0e17f2ea5629963a02a7f8
scott <qvis.be@gmail.com>
2020-07-16 17:45:30 +0800

    增加注释，删除不需要的字段

--- app/src/main/java/com/daleyzou/zhbj/domain/NewsMenu.java
+++ app/src/main/java/com/daleyzou/zhbj/domain/NewsMenu.java
@@ -8,17 +8,19 @@ import java.util.ArrayList;
  * 使用Gson解析时，对象书写技巧：
  *  1.逢{}创建对象，逢[]创建集合（ArrayList）
  *  2.所有字段要和json返回字段一致
+ *
  */
+//完整的api11-2.json的映射，包含retcode和extend和data三个主节点。
 public class NewsMenu {
-    public int retcode;
-    public ArrayList<Integer> extend;
     public ArrayList<NewsMenuData> data;
 
-    //侧边栏菜单对象
+    //侧边栏菜单对象，对应api11-2.json中的  "id": 10000,"title": "研值快报","type": 1，children,数组，目前就定义了第一个。
     public class NewsMenuData{
         public String id;
         public String title;
         public int type;
+
+        //例如：children在目前定义的"title": "研值快报",中是一个20个的一级分类。
         public ArrayList<NewsTabData> children;
 
         @Override
@@ -32,7 +34,7 @@ public class NewsMenu {
         }
     }
 
-    //页签的对象
+    //页签的对象，对应api11-2.json中的data,children。所以api21-XXX是没有定义常量在GlobalConstants.java中。
     public class NewsTabData{
         public String id;
         public String title;
